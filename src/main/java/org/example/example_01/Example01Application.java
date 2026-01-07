7d3aa4e2ea55e415d6b62cc9dca9f274c29f4667
Heshan <heshansandaruwan999@gmail.com>
2026-01-07 20:11:13 +0530

    Add ModelMapper configuration and Lombok dependency

--- src/main/java/org/example/example_01/Example01Application.java
+++ src/main/java/org/example/example_01/Example01Application.java
@@ -1,13 +1,20 @@
 package org.example.example_01;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Example01Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Example01Application.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 
 }
